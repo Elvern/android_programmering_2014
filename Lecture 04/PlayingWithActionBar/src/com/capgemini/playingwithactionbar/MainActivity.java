21ce81d69536f3e4fda6fa638059b14fe6d36fb8
Lars Emil Knudsen <larsemil87@gmail.com>
2014-02-08 18:19:14 +0100

    Added onClick to the MenuItems

--- Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
+++ Lecture 04/PlayingWithActionBar/src/com/capgemini/playingwithactionbar/MainActivity.java
@@ -1,7 +1,11 @@
 package com.capgemini.playingwithactionbar;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +14,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		
+		return true;
+	}
+	
+	public void onOption1Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 1", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onOption2Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 2", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onOption3Clicked(MenuItem menuItem) {
+		Toast.makeText(this, "Option 3", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onExitClicked(MenuItem menuItem) {
+		finish();
+	}
 }
